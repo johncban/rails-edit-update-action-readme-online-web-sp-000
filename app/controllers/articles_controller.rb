51b8a1c35d9edda7d67064da38c31594d34cb5c1
Juan Carlo Banayo <johncban@gmail.com>
2020-05-23 04:00:26 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    raise params.inspect
     @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
